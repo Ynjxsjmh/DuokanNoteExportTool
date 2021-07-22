fd522a4b0cada091eb343f9f6b7a8a3c6b19ce0f
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-07-22 21:40:27 +0800

    Add layer to connect with database

---
+++ src/connector.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+
+class Connector:
+    def __init__(self, db_path='../data/Bookshelf.db'):
+        self.con = sqlite3.connect(db_path)
+
+    def __del__(self):
+        self.con.close()
+
+    def find_annotations_in_book(self, book_id):
+        cur = self.con.cursor()
+
+        sql = '''
+        SELECT annotations.added_date AS added_date, annotation_type,
+               annotation_range, annotation_body, annotation_sample
+        FROM annotations
+        INNER JOIN books on annotations.book_id = books._id
+        WHERE books._id = ?;
+        '''
+
+        cur.execute(sql, (book_id,))
+
+        results = cur.fetchall()
+
+        return results
+
+    def find_book_by_id(self, book_id):
+        cur = self.con.cursor()
+
+        sql = '''
+        SELECT _id, book_name, author
+        FROM books
+        WHERE _id = ?;
+        '''
+
+        cur.execute(sql, (book_id,))
+
+        result = cur.fetchone()
+
+        return result
+
+    def find_all_books(self):
+        cur = self.con.cursor()
+
+        sql = '''
+        SELECT _id, book_name, author
+        FROM books;
+        '''
+
+        cur.execute(sql)
+
+        results = cur.fetchall()
+
+        return results
