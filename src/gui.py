fb9f016189fb8ef09e319cb782e170150d9b533f
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-08-23 22:16:51 +0800

    Resize gui

--- src/gui.py
+++ src/gui.py
@@ -140,6 +140,7 @@ class SettingDialog(QDialog):
 class DuoKanExportToolDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setFixedSize(640, 480)
 
         curDir = os.path.dirname(os.path.abspath(__file__))
         self.settingPath = os.path.join(curDir, 'setting.pickle')
